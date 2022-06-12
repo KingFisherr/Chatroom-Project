b9b779510e84793ad8e7187de8c6f539e30160b3
KingFisherr <tahsinprovath@aol.com>
2022-06-11 21:15:27 -0400

    Commited changes to base server file

--- server.py
+++ server.py
@@ -1,13 +1,33 @@
 import socket
+import threading
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((socket.gethostname(),1338))
+host = socket.gethostbyname()
+port = 1338
 
-s.listen()
-print ("Server open for connection...")
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+socket.bind((host, port))
+
+# Start listening
+socket.listen()
+
+clients = [] # or sockets
+nicknames_list = []
+
+# Need function to send message to all clients
+
+# Need function to handle messages from clients
+
+# Need function to receieve connection from client
 while True:
     clientconn, address = s.accept()
     print (f"Connection to {address} established...")
     with clientconn:
         clientconn.send(b"You have been connected to server!")
-    
+
+        
+# Need additional non core administrative functions or similiar
+
+#print ("Server open for connection...")
+# ready to receieve connection 
