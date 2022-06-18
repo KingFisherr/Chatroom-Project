d0e73ed9d4dcedcb84edfefb4c0b11385abe6f11
KingFisherr <tahsinprovath@aol.com>
2022-06-18 17:57:12 -0400

    Working on server issues s.0, s.1

--- client.py
+++ client.py
@@ -8,18 +8,30 @@ username = input("Choose live chat username: ")
 clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Connect to host server
-clientsocket.connect((socket.gethostname(), 1337)) #Host/port
+clientsocket.connect(("127.0.0.1", 1338)) #Host/port
 
 # Function to recieve data from server
 def recieve():
-    data = clientsocket.recv(1338)
-    print (data)
+    while True:    
+        try:
+            data = clientsocket.recv(1024).decode()
+            if data == "Username":
+                clientsocket.send(username.encode())
+                second_data = clientsocket.recv(1024).decode()
+                print(second_data)
+            else:
+                print(data)
+        except:
+            print ("Encountered some error")
+            clientsocket.close()
+            break
 
 # Function to send messages to server
-def chat():
-    d
+#def chat():
+    #d
 
-thread_recieve = threading.Thread(target=recieve)
-thread_recieve.start()
-thread_chat = threading.Thread(target=chat)
-thread_chat.start()
+recieve()
+#thread_recieve = threading.Thread(target=recieve)
+#thread_recieve.start()
+# thread_chat = threading.Thread(target=chat)
+# thread_chat.start()
--- server.py
+++ server.py
@@ -1,19 +1,19 @@
 import socket
 import threading
 
-host = socket.gethostbyname()
+host = "127.0.0.1"
 port = 1338
 
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-socket.bind((host, port))
+server.bind((host, port))
 
 # Start listening
-socket.listen()
+server.listen()
 
 clients = [] # or sockets
-nicknames_list = []
+username_list = []
 
 # Need function to receieve connection from client
 def receive():
@@ -21,16 +21,41 @@ def receive():
         clientconn, address = server.accept()
         print (f"Connection to {address} established...")
 
+        #Ask client for username
+        clientconn.send("Username".encode())
+
+        # Username var stores the username received from client
+        username = clientconn.recv(1024).decode()
+
+        # Maybe check ban database here 
+
+        # Maybe also check username/password database and implement a login system (admin will have different login)
+
+        # Update client list and username list with new client
+        clients.append (clientconn)
+        username_list.append(username)
+
+        print(f"Client's username is {username}")
+         
+        # Call broadcast func to send a message to all clients
+        # message will notify all clients of a newly joined client
+
+        clientconn.send("You are now connected to the live chat server".encode())
+
+        #Multiple client
+
+        #thread = threading.Thread(target= )
 
 # Need function to send message to all clients
-def broadcast(messsage):
-    for x in clients:
-        x.send(messsage)
+#def broadcast(messsage):
+    #for x in clients:
+        #x.send(messsage)
 
 # Need function to handle messages from clients
-def handler():
+#def handler():
     
 # Need additional non core administrative functions or similiar
 
-# print ("Server open for connection...")
+print ("Server open for connection")
+receive()
 # ready to receieve connection 
