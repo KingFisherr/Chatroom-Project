262cc0146c9cfa3048bf2bd161a5d6228f4b65d8
KingFisherr <tahsinprovath@aol.com>
2022-06-21 16:43:04 -0400

    Documentation for dbmodels.py

--- dbmodels.py
+++ dbmodels.py
@@ -2,17 +2,29 @@
 import os
 import sqlite3
 
-# We will implement multiple functions that allow us to use a sqllite database as a means to store encrypted user credentials
-
+# Class database gives access to methods used to access sqllite databases
 
 class database:
+
+    # Method returns all user info from user_database
     def checkdb(self):
+
+        # Connect to file
         conn = sqlite3.connect('user_database.sqlite') 
+        # Set cursor
         c = conn.cursor()
+        # SQL Query
         c.execute("SELECT username, password FROM userinfo")
+        # Get results
         print(c.fetchall())
 
+    # Method returns all user info from ban_database
+    #
+    #
+
+    # Method checks if user_database exists and if not creates it
     def checkfordb(self):
+
         if os.path.exists('user_database.sqlite'):
             print ("Database exists!")
             return
@@ -27,7 +39,14 @@ class database:
             
             print ("User Database created!")
 
+    # Method checks if ban_database exists and if not creates it
+    #
+    #
+
+
+    # Method checks if user login credentials are valid
     def checklogin(self, username, password):    
+
         conn = sqlite3.connect('user_database.sqlite')
         c = conn.cursor()
         statement = (f'''SELECT username from "userinfo" WHERE username="{username}" AND password = "{password}"''')
@@ -37,14 +56,23 @@ class database:
         else:
             print("Welcome")
 
+    # Method checks if user exists in ban_database
+    #
+    #
+
+    # Method to store new user credentials
     def storelogin(self, username, password):
+
         conn = sqlite3.connect('user_database.sqlite') 
         c = conn.cursor()    
-
         c.execute(f'''INSERT INTO "userinfo"(username, password) VALUES
                     ("{username}","{password}")''')
 
         conn.commit()       
+
+    # Method to store user info in ban_database
+    #
+    #
 
 db = database()
 # db.checkfordb()
