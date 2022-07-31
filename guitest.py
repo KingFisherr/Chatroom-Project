e9be181ad17fe7823aaf186685a1cd71831740c4
Jonathan Ibarra <jonathanibarra@IMPULSE.local>
2022-07-31 19:53:46 -0400

    some integration...

--- guitest.py
+++ guitest.py
@@ -1,95 +1,110 @@
 import json
 import socket
+import time
+
+import stdiomask
+import getpass
 import threading
-#from crypter import AESCrypter
-#from base64 import b64encode, b64decode
-from tkinter import *
+from crypter import AESCrypter
+from base64 import b64encode, b64decode
+import tkinter
 import tkinter.scrolledtext
 from tkinter import simpledialog
 
-
 HOST = "127.0.0.1"
 PORT = 1338
+
 
 class Client:
     def __init__(self, host, port):
 
         # Connect to server
-        self.clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.clientsocket = None
 
-        self.clientsocket.connect((host, port)) #Host/port
+        while True:
+            try:
+                # print(host, " : ", port)
+                self.clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                self.clientsocket.connect((host, port))  # Host/port
+                break
+            except Exception as e:
+                print("failed to connect to server ec {}".format(e))
+                time.sleep(1)
+                print("trying again..")
 
         # Create window for login
         msgbox = tkinter.Tk()
         msgbox.withdraw()
 
         # Create login elements
-        self.username = simpledialog.askstring("Username","Please enter nickname: ", parent = msgbox)
-        self.password = simpledialog.askstring("Password", "Enter password", parent = msgbox)
+        self.username = simpledialog.askstring("Username", "Please enter nickname: ", parent=msgbox)
+        self.password = simpledialog.askstring("Password", "Enter password", parent=msgbox)
 
         # Store username and password in json var
-        self.user_pass_json = (self.username,self.password)
+        self.user_pass_json = (self.username, self.password)
         self.user_pass_json = json.dumps(self.user_pass_json)
+
+        self.crypter = AESCrypter()
 
         # Set GUI_DONE to false and GUI_RUNNING to true
         self.gui_done = False
         self.gui_running = True
 
-        gui_thread = threading.Thread(target = self.gui_loop)
-        thread_recieve = threading.Thread(target= self.receive)
-        #thread_chat = threading.Thread(target= self.chat)
-
+        gui_thread = threading.Thread(target=self.gui_loop)
+        thread_receive = threading.Thread(target=self.receive)
+        # thread_chat = threading.Thread(target= self.chat)
 
         gui_thread.start()
-        thread_recieve.start()
-        #thread_chat.start()
+        thread_receive.start()
+        # thread_chat.start()
 
     # Create chat GUI window for client 
-        # Needs to look nicer
-        # Button for sending file
+    # Needs to look nicer
+    # Button for sending file
     def gui_loop(self):
         self.win = tkinter.Tk()
-        self.win.configure(bg = "lightgray")
+        self.win.configure(bg="lightgray")
 
-        self.chat_label = tkinter.Label(self.win, text="Chat:", bg = "lightgray")
+        self.chat_label = tkinter.Label(self.win, text="Chat:", bg="lightgray")
         self.chat_label.config(font=("Calibri,12"))
-        self.chat_label.pack(padx=20, pady= 5)
+        self.chat_label.pack(padx=20, pady=5)
 
         self.text_area = tkinter.scrolledtext.ScrolledText(self.win)
-        self.text_area.config(state = 'disabled')
-        self.text_area.pack(padx=20, pady= 5)
+        self.text_area.config(state='disabled')
+        self.text_area.pack(padx=20, pady=5)
 
-        self.message_label = tkinter.Label(self.win, text="Message:", bg = "lightgray")
+        self.message_label = tkinter.Label(self.win, text="Message:", bg="lightgray")
         self.message_label.config(font=("Calibri,12"))
-        self.message_label.pack(padx=20, pady= 5)
+        self.message_label.pack(padx=20, pady=5)
 
-        self.message_box = tkinter.Text(self.win, height= 3)
-        self.message_box.pack(padx=20, pady= 5)
-        
-        self.send_button = tkinter.Button(self.win, text= "Send", command = self.chat)
+        self.message_box = tkinter.Text(self.win, height=3)
+        self.message_box.pack(padx=20, pady=5)
+
+        self.send_button = tkinter.Button(self.win, text="Send", command=self.chat)
         self.send_button.config(font=("Calibri,12"))
-        self.send_button.pack(padx=20, pady= 5)
+        self.send_button.pack(padx=20, pady=5)
 
         self.gui_done = True
-
 
         # When window is closed we call end function
         self.win.protocol("WM_DELETE_WINDOW", self.end)
 
         # Binds return button to func
-        #self.win.bind('<Return>', self.chat)
+        # self.win.bind('<Return>', self.chat)
 
         self.win.mainloop()
-    
+
     def receive(self):
         while self.gui_running:
             try:
+                print("lmao what")
                 # Get data from server
                 data = self.clientsocket.recv(1024).decode()
 
                 print("RAW DATA {}".format(data))
                 if data == "Username":
                     self.clientsocket.send(self.user_pass_json.encode())
+                    print("jallo")
 
                 # If banned let client know, disconnect and end gui window
                 elif data == "Banned":
@@ -106,7 +121,7 @@ class Client:
                     self.gui_done = True
                     self.end()
                     thread_stopped = True
-                    
+
                 elif data == "Duplicate":
                     print("User is already logged in")
                     self.clientsocket.close()
@@ -114,48 +129,58 @@ class Client:
                     self.end()
                     thread_stopped = True
 
-                # elif data == "IV":
-                #     send_iv = b64decode(clientsocket.recv(24).decode())
-                #     recv_iv = b64decode(clientsocket.recv(24).decode())
-                #     # print(send_iv)
-                #     # print(recv_iv)
-                #     crypter.init_cipher(send_iv, recv_iv)   
-                #     print("iv has been initialized")
-                # elif data == "":
-                #     raise Exception("received empty string, server probably disconnected")
+                elif data == "IV":
+                    print("uhh")
+                    send_iv = b64decode(self.clientsocket.recv(24).decode())
+                    print("huh")
+                    recv_iv = b64decode(self.clientsocket.recv(24).decode())
+                    print(send_iv)
+                    print(recv_iv)
+                    self.crypter.init_cipher(send_iv, recv_iv)
+                    print("iv has been initialized")
+
+                elif data == "":
+                    raise Exception("received empty string, server probably disconnected")
 
                 else:
                     if self.gui_running:
-                        self.text_area.config(state = 'normal') 
+
+                        self.text_area.config(state='normal')
                         self.text_area.insert('end', data)
                         self.text_area.yview('end')
-                        self.text_area.config(state = 'disabled')
-                    #print("crypter initialized {}".format(crypter.initialized()))
-                    # if(crypter.initialized()):
-                    #     dmesg = crypter.decrypt_string(data)
-                    #     print(dmesg)
-                    # else:
-                    #     print("received message before crpter initialization : {}".format(data))
-            
-            except:
-                print ("Error connecting to server")
+                        self.text_area.config(state='disabled')
+
+                    elif self.crypter.initialized():
+                        # print("crypter initialized {}".format(self.crypter.initialized()))
+                        dmesg = self.crypter.decrypt_string(data)
+                        print(dmesg)
+                    else:
+                        print("received message before crypter initialization : {}".format(data))
+
+            except Exception as e:
+                print("Error connecting to server ec {}", e)
                 self.clientsocket.close()
                 break
-    
-    def chat(self, _event=None):     
+
+    def chat(self, _event=None):
         message = f"{self.username}: {self.message_box.get('1.0', 'end')}"
-        self.clientsocket.send(message.encode())
-        self.message_box.delete('1.0', 'end')          
 
+        emsg = self.crypter.encrypt_string(message)
+        # clientsocket.send(b64decode(dmsg).decode())
+        # clientsocket.send(emsg)
 
-    # Stop GUI and close client socket
+        self.clientsocket.send(emsg)
+        self.message_box.delete('1.0', 'end')
+
+        # Stop GUI and close client socket
+
     def end(self):
         self.clientsocket.close()
         self.gui_running = False
         self.win.destroy()
-        #self.clientsocket.close()
+        # self.clientsocket.close()
         exit(0)
 
-    
+
 # Initialize client object
 client = Client(HOST, PORT)
--- testserver.py
+++ testserver.py
@@ -1,9 +1,15 @@
-import socket
-import threading
-import json
 import re
-from crypter import AESCrypter
+import json
+import time
+import socket
+import bcrypt
+import threading
 from dbmodels import database
+from crypter import AESCrypter
+from base64 import b64encode, b64decode
+from Crypto.Random import get_random_bytes
+
+
 
 # Create database object from dbmodels
 db = database()
@@ -29,122 +35,180 @@ username_list = []
 # List of admins
 admins = []
 
-# Functions handles messages sent to server by clients
-def handler(client):
-
-    while True:
-        try:
-            # Get message from client
-            message = client.recv(1024)
-
-            #search for command
-            if re.search (r":\s/.*",str(message)):
-                commands(message, client)
-
-            # Broadcast message to all clients
-            else:
-                broadcast(message, client)
-
-            # Implement function or add on to this function for file transfer functionality
-            
-        except:
-            # Broadcast the user has disconnected
-            index = clients.index(client)
-            username = username_list[index]
-            client.close()
-            broadcast_disconnected(f"{username} has left the chat\n".encode(), client)
-
-            # Disconnect client from server and remove from list
-            clients.remove(client)
-            #client.close()
-            username_list.remove(username)
-            break
+# List of crypters
+crypters = []
 
 # Need function to receieve connection from client
 def receive():
     while True:
-        clientconn, address = server.accept()
-        print (f"Connection to {address} established...")
+       clientconn, address = server.accept()
+       print (f"Connection to {address} established...")
 
-        #Ask client for username
-        clientconn.send("Username".encode())
+       #Ask client for username
+       clientconn.send("Username".encode())
 
-        # user_pass_json var stores the username and password received from client as json
-        user_pass_json = clientconn.recv(1024).decode()
-        #print (user_pass_json)
-        user_pass_json = json.loads(user_pass_json)
+       # user_pass_json var stores the username and password received from client as json
+       user_pass_json = clientconn.recv(1024).decode()
+       #print (user_pass_json)
+       user_pass_json = json.loads(user_pass_json)
+       
+       print(user_pass_json)
+       print("brehhh")
 
-        username = user_pass_json[0]
-        password = user_pass_json[1]
+       username = user_pass_json[0]
+       password = user_pass_json[1]
 
-        # username = clientconn.recv(1024).decode()
-        # password = "banana"
+       # username = clientconn.recv(1024).decode()
+       # password = "banana"
 
-        # Check if user is already in server, by checking username/client ip list
-        if (ifuserexists(username)):
-            clientconn.send("Duplicate".encode())
-            clientconn.close()
-            continue
-        # Notes argument taken in to IP
+       # Check if user is already in server, by checking username/client ip list
+       if (ifuserexists(username)):
+           clientconn.send("Duplicate".encode())
+           clientconn.close()
+           continue
+       # Notes argument taken in to IP
+       print("breh2")
+       # Check for ban database
+       db.checkfordb('ban_database.sqlite')
+       # Check if client is banned
+       if db.checkban(username):
+           # If client is on ban list send them ban message
+           clientconn.send("Banned".encode())
+           # Disconnect client from server
+           clientconn.close()
+           continue
+    
+       print("3")
+       #init crypter
+       crypter = AESCrypter()
+       send_iv = get_random_bytes(16)
+       recv_iv = get_random_bytes(16)
+       crypter.init_cipher(send_iv, recv_iv)
+       
+       print("4")
 
-        # Check for ban database
-        db.checkfordb('ban_database.sqlite')
-        # Check if client is banned
-        if db.checkban(username):
-            # If client is on ban list send them ban message
-            clientconn.send("Banned".encode())
-            # Disconnect client from server
-            clientconn.close()
-            continue
+       clientconn.send("IV".encode())
+       time.sleep(1)
+       clientconn.send(b64encode(recv_iv))
+       clientconn.send(b64encode(send_iv))
+       
+       print("5")
+    
+       # NOTES
+       # Currently we can kick banned user when they connect to server via username and password, should just be socket (ip address)
+       # Need to fix login checker
+
+       # We have a username and password
+       # First check user_info database to see if given username exists
+           # If it exists we can verify both username and password
+           # Else we will store the new username and password
+
+
+       # Check for user database
+       db.checkfordb('user_database.sqlite')    
+       # Check if username exists
+       if db.checkUsername(username): 
+           print ("Username exists")
+           # If username exists then verify login
+           if not db.checklogin(username, password):
+               print("Password does not match")
+               # We want to disconnect client so they retry password
+               clientconn.send("Wrongpass".encode())
+               clientconn.close()
+               continue
+       else:
+           print ("stored user info")
+           hashed = bcrypt.hashpw(password.encode(), bcrypt.gensalt(13))
+           #cheking if DB exists before trying to store
+           db.checkfordb('user_database.sqlite')
+           db.storeuserinfo(username, hashed.decode())
+
+    
+
+       # Update client list and username list with new client
+       crypters.append(crypter)
+       clients.append(clientconn)
+       username_list.append(username)
+
+       print(f"{username} is joining the server")
+    
+       # Call broadcast func to send a message to all clients
+       broadcast(f"{username} has joined the server\n".encode(), clientconn)
+
+       # Let client know they are now connected to the chat server
+       # clientconn.send("You are now connected to the live chat server".encode())
+
+       cstr = crypter.encrypt_string("You are now connected to the live chat server")
+       clientconn.send(cstr)
+
+       # Handle multiple clients
+       handlerthread = threading.Thread(target=handler, args=(clientconn,))
+       handlerthread.start()
+
+# Functions handles messages sent to server by clients
+def handler(client):
+    
+    crypter = client_to_crypter(client)
+    
+    while True:
+        try:
+            # Get message from client
+            message = client.recv(1024)
+            
+            if(message == b""):
+                raise Exception("exception: received empty string") 
+            
+            # print("RECEIVED RAW {}".format(message))
+            dmsg = crypter.decrypt_string(message)
+            
+            print("received {}".format(dmsg))
+            
+            # Broadcast message to all clients
+            broadcast(dmsg.decode(), client)
+            
+            # Implement function or add on to this function for file transfer functionality
+            
+        except Exception as ex:
+            print(ex)
+            # Broadcast the user has disconnected
+            index = clients.index(client)
+            username = username_list[index]
+            broadcast(f'{username} has left the chat', client)
+            
+            # not thread safe, data race
+            # Disconnect client from server and remove from list
+            crypters.remove(crypter)
+            username_list.remove(username)
+            clients.remove(client)
+            client.close()
+            break
+
+
         
-        # NOTES
-        # Currently we can kick banned user when they connect to server via username and password, should just be socket (ip address)
-        # Need to fix login checker
-
-        # We have a username and password
-        # First check user_info database to see if given username exists
-            # If it exists we can verify both username and password
-            # Else we will store the new username and password
-
-
-        # Check for user database
-        db.checkfordb('user_database.sqlite')    
-        # Check if username exists
-        if db.checkUsername(username): 
-            print ("Username exists")
-            # If username exists then verify login
-            if not db.checklogin(username, password):
-                print("Password does not match")
-                # We want to disconnect client so they retry password
-                clientconn.send("Wrongpass".encode())
-                clientconn.close()
-                continue
-        else:
-            print ("Stored new user info")
-            db.storeuserinfo(username, password)
-
-        
-
-        # Update client list and username list with new client
-        clients.append (clientconn)
-        username_list.append(username)
-
-        print(f"{username} is joining the server")
-        
-        # Call broadcast func to send a message to all clients
-        broadcast(f"{username} has joined the server\n".encode(), clientconn)
-
-        # Let client know they are now connected to the chat server
-        # clientconn.send("You are now connected to the live chat server".encode())
-
-        # Handle multiple clients
-        handlerthread = threading.Thread(target=handler, args=(clientconn,))
-        handlerthread.start()
+def client_to_crypter(client):
+    # get the index of the target client
+    index = clients.index(client)
+    # get the crypter of the target client
+    return crypters[index]
 
 # Function sends message to all clients
-def broadcast(messsage, client):
+def broadcast(message, client):
+    print(f"broadcasting {message}")
     for x in clients:
-        x.send(messsage)
+        # skip the sender
+        if x == client:
+            continue
+            
+        try:
+            crypter = client_to_crypter(x)
+            # encrypt the message to send
+            emsg = crypter.encrypt_string(message)
+            # ( ( ( hacky sleep cuz no size header ) ) )
+            time.sleep(0.5)
+            # send the message to the target clientss
+            x.send(emsg)
+        except Exception as ex:
+            print(ex)
 
 # Function sends message only to all connected clients
 def broadcast_disconnected(messsage, client):
